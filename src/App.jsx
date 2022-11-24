6ceb91da71feb92fd93b1d7cd45184b222f86b4d
Robert Servado <servadorobertgmail.com>
2022-11-23 23:36:33 -0500

    Added src/App.jsx so that html shortcut commands can be used

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return <div>App</div>;
+  // <RestaurantsContextProvider>
+  //   <div className="container">
+  //     <BrowserRouter>
+  //       <Routes>
+  //         <Route path="/" element={<HomePage />} />
+  //         <Route path="/restaurants/:id" element={<RestaurantDetailPage />} />
+  //         <Route
+  //           path="/restaurants/:id/update"
+  //           element={<UpdateRestaurantPage />}
+  //         />
+  //       </Routes>
+  //     </BrowserRouter>
+  //   </div>
+  // </RestaurantsContextProvider>
+};
+
+export default App;
