6e6f0eb9436f3facf6550a1f931e62ed6f16b9b1
Roberto Cervantes <robertocervantes@Robertos-MBP-2.phub.net.cable.rogers.com>
2022-11-29 19:45:32 -0500

    add approved, pending, rejected menu

--- src/routes/PersonalRentalsPage.jsx
+++ src/routes/PersonalRentalsPage.jsx
@@ -7,23 +7,10 @@ import StuffIRented from "../components/My stuff/StuffIRented";
 import NewItemModal from "../components/My stuff/NewItemModal";
 
 const PersonalUserPage = () => {
-  const [itemData, setItemData] = useState([]);
+
   const [rentalData, setRentalData] = useState([]);
   const [showModal, setShowModal] = useState(false);
 
-  useEffect(() => {
-    const fetchItemData = async () => {
-      try {
-        const response = await StuffData.get("/users/2/items")
-        console.log("items:",response.data.data.items)
-        setItemData(response.data.data.items)
-      } catch(err){}
-    }
-
-    fetchItemData();
-  }, []);
-
-  console.log("item data:", itemData)
 
   useEffect(() => {
     const fetchRentalData = async () => {
@@ -37,58 +24,31 @@ const PersonalUserPage = () => {
     fetchRentalData();
   }, []);
 
-  const handleClick = (e) => {
-    setShowModal(true);
-  };
-
-  const addItem = (item) => {
-    setItemData([...itemData, item])
-  }
-
-  const closeModal = (value,item) => {
-    setShowModal(value);
-    // console.log("show modal2", showModal);
-    addItem(item)
-    // window.location.reload(false);
-  };
-
-  const handleDelete = async (id) => {
-    try{
-      const response = await StuffData.delete(`items/${id}/item`)
-      setItemData(itemData.filter(item => {
-        return item.id !== id
-      }))
-    } catch (err){
-
-    } 
-  }
 
   return (
     <>
-      {showModal && <NewItemModal closeModal={closeModal} />}
       <div class="bg-white">
-        <div class="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-          <h2 class="text-2xl font-bold tracking-tight text-gray-900">
-            My Stuff
-          </h2>
-
-          <button
-            onClick={handleClick}
-            class="mt-10 flex items-center justify-center rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-          >
-            Add item
-          </button>
-
-          <div class="whitespace-normal overflow-auto mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-            <MyListofStuff items={itemData} delete={handleDelete} />
-          </div>
-        </div>
+        
 
    <div class="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
     <h2 class="text-2xl font-bold tracking-tight text-gray-900">Stuff I've rented or want to rent</h2>
-    <form>
+    {/* <form>
     <button type="submit" class="mt-10 flex items-center justify-center rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Rent</button>
-    </form>
+    </form> */}
+    <div class="inline-flex rounded-md shadow-sm" role="group">
+  <button type="button" class="inline-flex items-center py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-l-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 ">
+    <p class="text-base">&#128077;</p>
+    &nbsp;Approved
+  </button>
+  <button type="button" class="inline-flex items-center py-2 px-4 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 ">
+  <p class="text-base">&#128528;</p>
+  &nbsp;Pending
+  </button>
+  <button type="button" class="inline-flex items-center py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-r-md border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 ">
+  <p class="text-base">&#128078;</p>
+  &nbsp;Rejected
+  </button>
+</div>
     <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
     <h2>Approved Rentals</h2>
     <StuffIRented 
